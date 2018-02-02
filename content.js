f23a3d88cfb882cb84286175f4806bc44d55a593
Nathan Koch <nathanrkoch@gmail.com>
2018-02-02 17:14:46 -0500

    accelerates

--- content.js
+++ content.js
@@ -1,23 +1,37 @@
 // content.js
-var clickedIn = false;
 var $logoLinks = $('a._19eb');
+var delay = 5000;
+var interval;
 
-function scrollRandomly() {
+function getRandomStory() {
     var $allStories = $('[data-testid="fbfeed_story"]');
     var randomStoryIndex = Math.floor(Math.random() * $allStories.length);
-    var $randomStory = $allStories.eq(randomStoryIndex);
-    $('html, body').animate({ scrollTop: $randomStory.offset().top}, 2000, function() {
-        if (clickedIn) {
-            $logoLinks[0].click();
-        } else {
-            var $storyLinks = $randomStory.find("a");
-            var randomLinkIndex = Math.floor(Math.random() * $storyLinks.length);
-            var $randomLink = $storyLinks.eq(randomLinkIndex);
-            $randomLink.css("background-color", "#dddddd");
-            clickedIn = true;
-            $randomLink[0].click();
-        }
-    });
+    return $allStories.eq(randomStoryIndex);
 }
 
-setInterval(scrollRandomly, 5000);
+function getRandomLink($randomStory) {
+    var $storyLinks = $randomStory.find("a");
+    var randomLinkIndex = Math.floor(Math.random() * $storyLinks.length);
+    return $storyLinks.eq(randomLinkIndex);
+}
+
+function scrollRandomly() {
+    clearInterval(interval);
+    if (delay > 500) {
+        delay = delay - 100;
+    }
+    interval = setInterval(scrollRandomly, delay);
+    var $randomStory = getRandomStory();
+
+    if ($('body').hasClass('composerExpanded')) {
+        $('html, body').animate({ scrollTop: $randomStory.offset().top}, 2000, function() {
+            var $randomLink = getRandomLink($randomStory);
+            $randomLink.css("background-color", "#dddddd");
+            $randomLink[0].click();
+        });
+    } else {
+        $logoLinks[0].click();
+    }
+}
+
+interval = setInterval(scrollRandomly, delay);
