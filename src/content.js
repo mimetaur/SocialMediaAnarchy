c2d05b31d544f633c8e692819693eefc5f8d9a2a
Nathan Koch <nathanrkoch@gmail.com>
2018-02-09 16:45:46 -0500

    added an explicit like button mode

--- src/content.js
+++ src/content.js
@@ -1,37 +1,56 @@
 // content.js
 var $logoLinks = $('a._19eb');
-var delay = 5000;
-var interval;
+var storySelector = '[data-testid="fbfeed_story"]';
+var gifSelector = 'a[aria-label="Post a GIF"]';
+var emojiSelector = 'a[aria-label="Insert an emoji"]';
+var likeButtonSelector = 'a[data-testid="fb-ufi-likelink"]';
+var unlikeButtonSelector = 'a[data-testid="fb-ufi-unlikelink"]';
+var delay = 8000;
 
 function getRandomStory() {
-    var $allStories = $('[data-testid="fbfeed_story"]');
+    var $allStories = $(storySelector);
     var randomStoryIndex = Math.floor(Math.random() * $allStories.length);
     return $allStories.eq(randomStoryIndex);
 }
 
-function getRandomLink($randomStory) {
-    var $storyLinks = $randomStory.find("a");
+function getRandomLink($story) {
+    var $storyLinks = $story.find("a");
     var randomLinkIndex = Math.floor(Math.random() * $storyLinks.length);
     return $storyLinks.eq(randomLinkIndex);
 }
 
-function scrollRandomly() {
-    clearInterval(interval);
-    if (delay > 500) {
-        delay = delay - 100;
-    }
-    interval = setInterval(scrollRandomly, delay);
-    var $randomStory = getRandomStory();
+function performRandomAction($story) {
+    var probability = Math.random();
 
+    if (probability < 0.9) { // click the like/unlike button
+        var likeButtonEl = $story.find(likeButtonSelector)[0];
+        var unlikeButtonEl = $story.find(likeButtonSelector)[0];
+        if (!!likeButtonEl) {
+            console.log("Clicking the like button");
+            console.log(likeButtonEl);
+            likeButtonEl.click();
+        } else if (!!unlikeButtonEl) {
+            console.log("Clicking the unlike button");
+            console.log(unlikeButtonEl);
+            unlikeButtonEl.click();
+        }
+    } else {
+        console.log("Doing something else");
+        // var $randomLink = getRandomLink($story);
+        // $randomLink[0].click();
+    }
+
+}
+
+function scrollRandomly() {
     if ($('body').hasClass('composerExpanded')) {
-        $('html, body').animate({ scrollTop: $randomStory.offset().top}, 2000, function() {
-            var $randomLink = getRandomLink($randomStory);
-            $randomLink.css("background-color", "#dddddd");
-            $randomLink[0].click();
+        var $randomStory = getRandomStory();
+        $('html, body').animate({ scrollTop: $randomStory.offset().top }, 2000, function () {
+            performRandomAction($randomStory);
         });
     } else {
         $logoLinks[0].click();
     }
 }
 
-interval = setInterval(scrollRandomly, delay);
+var interval = setInterval(scrollRandomly, delay);
